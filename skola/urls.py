04d0e697c2ba6594184ebdf2f1fbf3d603bd7fb6
MoriKen <a.ladnak25@gmail.com>
2023-12-01 11:38:56 +0100

    index

--- skola/urls.py
+++ skola/urls.py
@@ -3,5 +3,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.vypis_students, name='vypis-students'),
+    path('', views.vypis_skola, name='vypis-skola'),
+    path('triedy/', views.vypis_triedy, name="vypis-triedy"),
+    path('ucitelia/', views.vypis_ucitelov, name="vypis-ucitelov"),
+    path('studenti/', views.vypis_students, name="vypis-students"),
 ]
